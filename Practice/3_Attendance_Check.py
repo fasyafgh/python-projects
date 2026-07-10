6b85c5ce198932a2a7541553c4e740047207e2ef
fasyafgh <fasya1996@gmail.com>
2026-07-10 20:19:27 +0700

    Bug fix on early condition and cosmetics

--- Practice/3_Attendance_Check.py
+++ Practice/3_Attendance_Check.py
@@ -13,14 +13,14 @@ max_time = datetime.strptime("07:00",fmt)
 #Define variable arrival time
 arrival_time = datetime.strptime(input_arrival,fmt)
 
-#Calculate deviation from max_time
-minutes_late = (arrival_time-max_time).total_seconds()/60
-minutes_early = (max_time-arrival_time).total_seconds()/60
-
 #Condition to sort attendance
-if arrival_time>max_time:
+if arrival_time > max_time:
+    #Calculate deviation from max_time
+    minutes_late = (arrival_time-max_time).total_seconds()/60
     print(f"Late by {minutes_late:.0f} minutes")
-elif arrival_time==max_time:
+elif arrival_time == max_time:
     print("On Time")
 else:
-    print(f"Late by {minutes_early:.0f} minutes")
+    #Calculate deviation from max_time
+    minutes_early = (max_time-arrival_time).total_seconds()/60
+    print(f"Early by {minutes_early:.0f} minutes")
